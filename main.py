3681e7178156aa5672487bb92dfd519cdaa24310
Tuukka Haapasalo <tuukka.haapasalo@reaktor.com>
2023-10-12 16:37:38 +0300

    Trying out PG integration

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
+import asyncpg
+
 from typing import Union
+from config import DATABASE_URL
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from pydantic import BaseModel
+from sqlalchemy.orm import Session
 
+
+async def create_db_pool():
+    return await asyncpg.create_pool(DATABASE_URL)
+
+
+db_pool = create_db_pool()
 app = FastAPI()
+
+
+def get_db():
+    db = db_pool.acquire()
+    yield db
+    db.release()
 
 
 class Item(BaseModel):
@@ -25,3 +41,18 @@ def read_item(item_id: int, q: Union[str, None] = None):
 @app.put("/items/{item_id}")
 def update_item(item_id: int, item: Item):
     return {"item_name": item.name, "item_id": item_id}
+
+
+@app.post("/items")
+async def update_item(item: Item):
+    # Take a connection from the pool.
+    async with db_pool.acquire() as connection:
+        # Open a transaction.
+        async with connection.transaction():
+            item = await connection.execute(
+                "INSERT INTO items (name, price) VALUES ($1, $2) RETURNING *",
+                item.name,
+                item.price,
+            ).fetchone()
+            print("Inserted item", item)
+            return {"status": "ok"}
