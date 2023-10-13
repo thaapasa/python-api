6bc71b372ffba32f8b3ca8f8370a2b43a621cfb6
Tuukka Haapasalo <tuukka.haapasalo@reaktor.com>
2023-10-13 09:02:48 +0300

    Use Depends to inject transactions to route defs

--- main.py
+++ main.py
@@ -3,13 +3,21 @@ import asyncpg
 from typing import Union
 from config import DATABASE_URL
 
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Depends
 from pydantic import BaseModel
 from sqlalchemy.orm import Session
+
+from contextlib import asynccontextmanager
 
 
 pool = None
 app = FastAPI()
+
+
+async def get_transaction() -> asyncpg.Connection:
+    async with pool.acquire() as connection:
+        async with connection.transaction():
+            yield connection
 
 
 @app.on_event("startup")
@@ -24,12 +32,6 @@ async def shutdown_event():
     await pool.close()
 
 
-def get_db():
-    db = pool.acquire()
-    yield db
-    db.release()
-
-
 class Item(BaseModel):
     name: str
     price: float
@@ -42,75 +44,57 @@ def read_root():
 
 
 @app.get("/items")
-async def read_item():
-    # Take a connection from the pool.
-    async with pool.acquire() as connection:
-        # Open a transaction.
-        async with connection.transaction():
-            items = await connection.fetch(
-                "SELECT * FROM items",
-            )
-            print("Found items", items)
-            return {"items": items, "status": "ok"}
+async def read_item(tx=Depends(get_transaction)):
+    items = await tx.fetch(
+        "SELECT * FROM items",
+    )
+    print("Found items", items)
+    return {"items": items, "status": "ok"}
 
 
 @app.get("/items/{item_id}")
-async def read_item(item_id: int, q: Union[str, None] = None):
-    # Take a connection from the pool.
-    async with pool.acquire() as connection:
-        # Open a transaction.
-        async with connection.transaction():
-            item = await connection.fetchrow("SELECT * FROM items WHERE id=$1", item_id)
-            if item is None:
-                raise HTTPException(status_code=404, detail="Item not found")
-            print("Found item", item)
-            return {"item": item, "status": "ok", "q": q}
+async def read_item(
+    item_id: int, q: Union[str, None] = None, tx=Depends(get_transaction)
+):
+    item = await tx.fetchrow("SELECT * FROM items WHERE id=$1", item_id)
+    if item is None:
+        raise HTTPException(status_code=404, detail="Item not found")
+    print("Found item", item)
+    return {"item": item, "status": "ok", "q": q}
 
 
 @app.put("/items/{item_id}")
-async def update_item(item_id: int, item: Item):
-    # Take a connection from the pool.
-    async with pool.acquire() as connection:
-        # Open a transaction.
-        async with connection.transaction():
-            item = await connection.fetchrow(
-                "UPDATE items SET name=$2, price=$3 WHERE id=$1 RETURNING *",
-                item_id,
-                item.name,
-                item.price,
-            )
-            if item is None:
-                raise HTTPException(status_code=404, detail="Item not found")
-            print("Updated item", item)
-            return {"status": "ok", "item": item}
+async def update_item(item_id: int, item: Item, tx=Depends(get_transaction)):
+    item = await tx.fetchrow(
+        "UPDATE items SET name=$2, price=$3 WHERE id=$1 RETURNING *",
+        item_id,
+        item.name,
+        item.price,
+    )
+    if item is None:
+        raise HTTPException(status_code=404, detail="Item not found")
+    print("Updated item", item)
+    return {"status": "ok", "item": item}
 
 
 @app.post("/items")
-async def update_item(item: Item):
-    # Take a connection from the pool.
-    async with pool.acquire() as connection:
-        # Open a transaction.
-        async with connection.transaction():
-            item = await connection.fetchrow(
-                "INSERT INTO items (name, price) VALUES ($1, $2) RETURNING *",
-                item.name,
-                item.price,
-            )
-            print("Inserted item", item)
-            return {"status": "ok", "item": item}
+async def update_item(item: Item, tx=Depends(get_transaction)):
+    item = await tx.fetchrow(
+        "INSERT INTO items (name, price) VALUES ($1, $2) RETURNING *",
+        item.name,
+        item.price,
+    )
+    print("Inserted item", item)
+    return {"status": "ok", "item": item}
 
 
 @app.delete("/items/{item_id}")
-async def delete_item(item_id: int):
-    # Take a connection from the pool.
-    async with pool.acquire() as connection:
-        # Open a transaction.
-        async with connection.transaction():
-            item = await connection.fetchrow(
-                "DELETE FROM items WHERE id=$1 RETURNING *",
-                item_id,
-            )
-            if item is None:
-                raise HTTPException(status_code=404, detail="Item not found")
-            print("Deleted item", item)
-            return {"status": "ok", "deleted": item}
+async def delete_item(item_id: int, tx=Depends(get_transaction)):
+    item = await tx.fetchrow(
+        "DELETE FROM items WHERE id=$1 RETURNING *",
+        item_id,
+    )
+    if item is None:
+        raise HTTPException(status_code=404, detail="Item not found")
+    print("Deleted item", item)
+    return {"status": "ok", "deleted": item}
